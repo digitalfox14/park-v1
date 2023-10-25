3350bd670963949beb36317f9510bec0ce91c0ea
Digitalfox14 <webartisan.digitalfox@gmail.com>
2023-10-25 11:00:10 +0530

    Template integrated

---
+++ resources/views/layouts/foooter.blade.php
@@ -0,0 +1,10 @@
+<div id="page-wrapper" class="gray-bg" style="min-height: 127px;">
+  <div class="footer">
+    <div class="pull-right">
+        10GB of <strong>250GB</strong> Free.
+    </div>
+    <div>
+      <strong>Copyright</strong> Example Company © 2014-2017
+    </div>
+  </div>
+</div>
